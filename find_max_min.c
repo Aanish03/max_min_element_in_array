31fc9d0bd3046bd120c1efba5df6df2b6debe078
Aanish rajendra Deshmukh <83102459+anish936@users.noreply.github.com>
2022-04-13 08:53:19 +0530

    Add files via upload

---
+++ find_max_min.c
@@ -0,0 +1,80 @@
+#include<stdio.h> 
+
+int main(){
+
+    int num[10];
+      int bignumber = 0;
+      int i = 0;
+      int minnumber = num[0];
+
+
+    for( i=0; i != 10;i++){
+        
+
+        printf("enter number to store in array :  ");
+        scanf("%d",&num[i]);
+
+        
+        
+    }
+
+    for( i=0; i != 10;i++){
+
+
+      
+
+        if(num[i] > bignumber){
+            bignumber = num[i];
+
+        }
+        else{
+
+            
+        }
+
+
+    }
+
+for( i=0; i != 10;i++){
+
+
+      
+
+        if(num[i] > bignumber){
+            bignumber = num[i];
+
+        }
+        else{
+
+            
+        }
+
+
+    }
+for( i=0; i != 10;i++){
+
+
+      
+
+        if(num[i] < minnumber){
+            minnumber = num[i];
+
+        }
+        else{
+
+            
+        }
+
+
+    }
+
+
+
+
+    printf("the big number is  %d \n",bignumber);
+    printf("the min number is %d  \n" , minnumber);
+
+
+    
+    
+    }
